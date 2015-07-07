5f4ba31a4e4107e89e87fcaac27fa9bdc8a398e8
leekangtaqi <leekangtaqi@sina.com>
2015-07-07 17:39:17 +0800

    model change

--- src/modules/qrchannel/common/QrHandlerDispatcher.js
+++ src/modules/qrchannel/common/QrHandlerDispatcher.js
@@ -27,34 +27,9 @@ QrHandlerDispatcher.prototype.genKey = function(forever, type){
     return (forever ? 'fv' : 'tm') + type;
 };
 
-//var env = require('settings').domain;
-var strategiesMap = {
-    'fs' : '欢迎关注"种子旅行"，以后找活动、发活动就来这里吧...\n\n<a href=\"http://www.zz365.com.cn/mc/firstsubscribe?mcuid=%%?%%\">点击领取红包...</a>',
-    'sm' : '将抢红包活动分享至"朋友圈",你可以再获取一个红包呦:)\n\n<a href=\"http://www.zz365.com.cn/mc/sharemc?mcuid=%%?%%\">点击分享活动...</a>',
-    'uf': ''
-};
 QrHandlerDispatcher.prototype.dispatch = function(message, user, res){
     var reply = "", mc, mcu, user = user;
     var me = this;
-    MarketingCampaignService.getMCUAsync(user)
-        .then(function(doc){
-            mc = doc.mc;
-            mcu = doc.mcu;
-            for(var i = 0, len = mc.strategies.length; i < len; i++){
-                console.log('==================================================');
-                console.log(mc.strategies[i]);
-                console.log(MarketingCampaignStrategy.FirstSubscribe.value());
-                console.log((mc.strategies[i].name === MarketingCampaignStrategy.FirstSubscribe.value()) && (user.subscribeCount == 0));
-                if((mc.strategies[i].name === MarketingCampaignStrategy.FirstSubscribe.value()) && (user.subscribeCount == 0)){
-                    reply = u.appendLine(reply, strategiesMap[mc.strategies[i].name], mcu._id);
-                }else if(mc.strategies[i].name !== MarketingCampaignStrategy.FirstSubscribe.value()) {
-                    reply = u.appendLine(reply, strategiesMap[mc.strategies[i].name], mcu._id);
-                }
-            };
-            (reply.length >= 2) && (reply = reply.substring(0, reply.length-2));
-            console.log("-----------------" + reply);
-            reply.trim() == '' && (reply = "哇哦~太好了！我们又多了一个新种子，你好哇/::D\n\n这里是喊人一起玩儿的好地方哦！看看身边的朋友都在玩儿什么，赶紧加入吧！你也可以出个点子，叫上朋友一起动起来！\n\n<a href=\"http://www.zz365.com.cn\">点此发布活动或者报名参加！</a>");
-
             if(!message.EventKey){
                 me.defaultHandler && me.defaultHandler(message, user, res, reply, null);
             }
@@ -76,7 +51,7 @@ QrHandlerDispatcher.prototype.dispatch = function(message, user, res){
                     }
                 });
             }
-        })
+
 };
 
 
