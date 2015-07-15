117c1f92d6fb7669af4691e1dcb78dd9c7290107
node-rookie <749924911@qq.com>
2015-07-15 17:09:35 +0800

    registry bind student handler

--- src/modules/qrchannel/index.js
+++ src/modules/qrchannel/index.js
@@ -2,7 +2,7 @@ var QrHandlerDispatcher = require('./common/QrHandlerDispatcher');
 
 var dispatcher = new QrHandlerDispatcher();
 
-//dispatcher.register(require('./handlers/MarketingChannelHandler'));
+dispatcher.register(require('./handlers/StudentBindClazzHandler'));
 dispatcher.register(require('./handlers/TeacherSubscribeHandler'));
 //dispatcher.setNullHandler(require('./handlers/NullHandler'));
 //dispatcher.setDefaultHandler(require('./handlers/DefaultHandler'));
