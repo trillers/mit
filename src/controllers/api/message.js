02de02262ca5b78d52300d4ef4ca8e406af1bc71
node-rookie <749924911@qq.com>
2015-07-25 15:22:27 +0800

    update

--- src/controllers/api/message.js
+++ src/controllers/api/message.js
@@ -179,7 +179,7 @@ module.exports = function(router){
         saveMessageAsync(msg)
             .then(function(doc){
                 result = doc;
-                return  clazzService.loadStudentsByIdAsync(clazzId);
+                return  clazzService.loadStudentsByIdAsync(msg.channel);
             })
             .then(function(students){
                 var arr = [];
@@ -194,7 +194,7 @@ module.exports = function(router){
             })
             .catch(Error, function(err) {
                 logger.error('send mass message error: ' + err);
-                res.status(500).json(ApiReturn.i().errMsg(500, 'failed to send mass msg'));
+                res.status(500).json(ApiReturn.i().error(500, 'failed to send mass msg'));
             })
     });
     //save message
