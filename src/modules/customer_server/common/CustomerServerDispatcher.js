608b957afa5483abc3704e6eecf2f18421872a76
node-rookie <749924911@qq.com>
2015-08-07 09:59:52 +0800

    update

--- src/modules/customer_server/common/CustomerServerDispatcher.js
+++ src/modules/customer_server/common/CustomerServerDispatcher.js
@@ -1,59 +1,84 @@
 var csskv = require('../kvs/CustomerServer');
+var redis = require('redis');
 
 var CustomerServerDispatcher = function(){
     this.customerServers = {};
-    this.customerServerload = {}
+    this.subscribeClient = redis.createClient();
+    this.publishClient = redis.createClient();
+    this.redisClientInit();
 }
 
 var prototype  = CustomerServerDispatcher.prototype;
 
+prototype.redisClientInit = function(){
+    this.subscribeClient.on('subscribe', function(){
+        console.log('redis client had subscribed');
+    });
+    this.subscribeClient.subscribe('customer server message');
+    this.subscribeClient.on('message', this.handleMessage);
+}
+
+prototype.handleMessage = function(channel, message){
+    //TODO
+    var msg = JSON.parse(message);
+    var csId = msg.csId;
+    var cs = this.getCustomerServerById(csId);
+    if(cs){
+        cs.emit('message', msg);
+    }
+};
+
+prototype.publishMessage = function(csId, msg){
+    msg.csId = csId;
+    msg.customer = user;
+    this.publishClient.publish('customer server message', JSON.stringify(message));
+}
+
 prototype.registryCustomerServer = function(csId, cs){
     this.customerServers[csId] = cs;
-    this.customerServerload[csId] = 0;
+    csskv.setCSLoadById(csId, function(){
+        //TODO
+    });
 }
 
 prototype.delCustomerServer = function(csId){
-    delete this.customerServerload[csId];
     delete this.customerServers[csId];
 }
 
 prototype.getCustomerServerById = function(csId){
-    this.customerServerload[csId]++;
+    csskv.modifyCSLoadById(csId, 1, function(){
+        //TODO
+    });
     return this.customerServers[csId];
 }
 
 prototype.getLightLoadCustomerServerId = function(){
-    var key, load = 0;
-    var csLoad = this.customerServerload;
-    for(k in csLoad){
-        if(csLoad[k] >= load){
-            key = k;
-        }
-    }
-
-    return key;
+    var key, load = 100000;
+    csskv.loadCSLoadAsync()
+        .then(function(err, csLoad){
+            for(k in csLoad){
+                if(csLoad[k] <= load){
+                    key = k;
+                }
+            }
+            return key;
+        });
 }
 
 prototype.dispatch = function(user, message, csId){
-    var cs;
     var self = this;
     if(csId){
-        cs = self.getCustomerServerById(csId);
-        return cs.emit('message', {msg:message, user:user});
+        return this.publishMessage(csId, user, message);
     } else {
         csskv.loadCSSByOpenIdAsync(user.wx_openid)
             .then(function(csId){
-                console.log(csId);
                 if(csId){
-                    cs = self.customerServers[csId];
-                    return cs.emit('message', {msg:message, user:user});
+                    return this.publishMessage(csId, user, message);
                 }
-                var key = self.getLightLoadCustomerServerId();
-                console.log(key);
-                cs = self.getCustomerServerById(key);
-                csskv.saveCSSByOpendIdAsync(user.wx_openid, key)
+                var csId = self.getLightLoadCustomerServerId();
+                csskv.saveCSSByOpendIdAsync(user.wx_openid, csId)
                     .then(function(){
-                        return cs.emit('message', {msg:message, user:user});
+                        return this.publishMessage(csId, user, message);
                     })
             })
     }
--- src/modules/customer_server/kvs/CustomerServer.js
+++ src/modules/customer_server/kvs/CustomerServer.js
@@ -4,52 +4,52 @@ var _ = require('underscore');
 var Promise = require('bluebird');
 var cbUtil = require('../../../framework/callback');
 
-var getCSKey = function(){
-    return 'cs';
+var idToCSStatusKey = function(id){
+    return 'cs:st:' + id;
 };
 
-var idToCSKey = function(id){
-    return 'cs:' + id;
+var idToCSHandingSetKey = function(id){
+    return 'cs:set:' + id;
 }
 
-var idToCSField = function(id){
-    return 'cs' + id;
+var getCSLoadKey = function(){
+    return 'cs:load';
 }
 
 var openIdToCSSKey = function(openId){
-    return 'cs:session:' + openId;
+    return 'cs:sess:' + openId;
 }
 
 var CustomerServer = {
-    loadCSById: function(id, callback){
-        var key = idToCSKey(id);
-        redis.hgetall(key, function(err, result){
+    loadCSStatusById: function(id, callback){
+        var key = idToCSStatusKey(id);
+        redis.get(key, function(err, result){
             cbUtil.logCallback(
                 err,
-                'Fail to load customer server by id ' + id + ': ' + err,
-                'Succeed to load customer server by id ' + id);
+                'Fail to load customer server status by id ' + id + ': ' + err,
+                'Succeed to load customer server status by id ' + id);
             cbUtil.handleSingleValue(callback, err, result);
         });
     },
 
-    saveCSById: function(id, cs, callback){
-        var key = idToCSKey(id);
-        redis.hmset(key, cs, function(err, result){
+    saveCSStatusById: function(id, st, callback){
+        var key = idToCSStatusKey(id);
+        redis.set(key, st, function(err, result){
             cbUtil.logCallback(
                 err,
-                'Fail to save customer server by id: ' + id + ': ' + err,
-                'Succeed to save customer server by id: ' + id);
-            cbUtil.handleOk(callback, err, result);
+                'Fail to save customer server status by id: ' + id + ': ' + err,
+                'Succeed to save customer server status by id: ' + id);
+            cbUtil.handleOk(callback, err, result, st);
         });
     },
 
-    deleteCSById: function(id, callback){
-        var key = idToCSField(id);
+    delCSStatusById: function(id, callback){
+        var key = idToCSStatusKey(id);
         redis.del(key, function(err, result){
             cbUtil.logCallback(
                 err,
-                'Fail to delete customer server by id ' + id + ': ' + err,
-                'Succeed to delete customer server by id ' + id);
+                'Fail to delete customer server status by id ' + id + ': ' + err,
+                'Succeed to delete customer server by status id ' + id);
 
             cbUtil.handleSingleValue(callback, err, result);
         });
@@ -57,40 +57,130 @@ var CustomerServer = {
 
     loadCSSByOpenId: function(openId, callback){
         var key = openIdToCSSKey(openId);
-        redis.get(key, function(err, result){
+        redis.hgetall(key, function(err, result){
             cbUtil.logCallback(
                 err,
-                'Fail to load customer server session by customer openId ' + openId + ': ' + err,
-                'Succeed to load customer server session by customer openId ' + openId);
+                'Fail to load customer server session by openId ' + openId + ': ' + err,
+                'Succeed to load customer server session by openId ' + openId);
             cbUtil.handleSingleValue(callback, err, result);
         });
     },
 
-    saveCSSByOpendId: function(openId, csId, callback){
+    saveCSSByOpenId: function(openId, css, callback){
         var key = openIdToCSSKey(openId);
-        redis.set(key, csId, function(err, result){
+        redis.hmset(key, css, function(err, result){
             cbUtil.logCallback(
                 err,
-                'Fail to save customer server session by customer openId: ' + openId + ': ' + err,
-                'Succeed to save customer server session by customer openId: ' + openId);
-            cbUtil.handleOk(callback, err, result, csId);
+                'Fail to save customer server session by openId: ' + openId + ': ' + err,
+                'Succeed to save customer server session by openId: ' + openId);
+            cbUtil.handleOk(callback, err, result, css);
         });
     },
 
-    deleteCSSByOpenId: function(openId, callback){
+    delCSSByOpenId: function(openId, callback){
         var key = openIdToCSSKey(openId);
         redis.del(key, function(err, result){
             cbUtil.logCallback(
                 err,
-                'Fail to delete customer server session by customer openId ' + openId + ': ' + err,
-                'Succeed to delete customer server session by customer openId ' + openId);
+                'Fail to delete customer server session by openId ' + openId + ': ' + err,
+                'Succeed to delete customer server session by openId ' + openId);
 
             cbUtil.handleSingleValue(callback, err, result);
         });
-    }
+    },
+
+    loadCSHandingSetById: function(id, callback){
+        var key = idToCSHandingSetKey(id);
+        redis.smembers(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to load customer server handing set by id ' + id + ': ' + err,
+                'Succeed to load customer server handing set by id ' + id);
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    pushCSHandingSetById: function(id, openId, callback){
+        var key = idToCSHandingSetKey(id);
+        redis.sadd(key, openId, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to add customer server handing set by id: ' + id + ': ' + err,
+                'Succeed to add customer server handing set by id: ' + id);
+            cbUtil.handleAffected(callback, err, openId, result);
+        });
+    },
+
+    popCSHandingSetById: function(id, openId, callback){
+        var key = idToCSHandingSetKey(id);
+        redis.srem(key, openId, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to pop customer server handing set by id ' + id + ': ' + err,
+                'Succeed to pop customer server handing set status id ' + id);
+
+            cbUtil.handleAffected(callback, err, openId, result);
+        });
+    },
+
+    delCSHandingSetById: function(id, callback){
+        var key = idToCSHandingSetKey(id);
+        redis.del(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to delete customer server handing set by id ' + id + ': ' + err,
+                'Succeed to delete customer server handing set by id ' + id);
+
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    loadCSLoad: function(callback){
+        var key = getCSLoadKey();
+        redis.hgetall(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to load customer server load ' + err,
+                'Succeed to load customer server load');
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    setCSLoadById: function(id, callback){
+        var key = getCSLoadKey();
+        redis.hset(key, id, 0, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to set customer server load ' + err,
+                'Succeed to set customer server load');
+            cbUtil.handleAffected(callback, err, id, result);
+        });
+    },
+
+    modifyCSLoadById: function(id, num, callback){
+        var key = getCSLoadKey();
+        redis.hincrby(key, id, num, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to modify customer server by id: ' + id + ': ' + err,
+                'Succeed to save customer server by id: ' + id);
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    delCSLoadById: function(id, callback){
+        var key = getCSLoadKey();
+        redis.hdel(key, id, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to delete customer server load by id: ' + id + 'err:' + err,
+                'Succeed to delete customer server load by id:' + id );
+
+            cbUtil.handleAffected(callback, err, id, result);
+        });
+    },
 
 };
-
 
 CustomerServer = Promise.promisifyAll(CustomerServer);
 
